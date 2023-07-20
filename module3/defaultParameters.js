2d3bab794480435d9991ade43f22bb54700b7062
MarcoRuizz <mruiz22@ucol.mx>
2023-07-19 23:29:13 -0600

    OOP submodule finished, default parameters, opp program exercise

---
+++ module3/defaultParameters.js
@@ -0,0 +1,26 @@
+// allow me to build a function that will run with default argument values even if I don't pass it any arguments
+function withDefaultParams(number = 10) {
+  console.log("Result:", number * number);
+}
+
+withDefaultParams(); // 100
+
+// exammple with a class
+class WithDefaultParams {
+  constructor(num1 = 1, num2 = 2, num3 = 3, string1 = "Result:", bool1 = true) {
+    this.num1 = num1;
+    this.num2 = num2;
+    this.num3 = num3;
+    this.string1 = string1;
+    this.bool1 = bool1;
+  }
+  calculate() {
+    if (this.bool1) {
+      console.log(this.string1, this.num1 + this.num2 + this.num3);
+      return;
+    }
+    return "The value of bool1 is incorrect";
+  }
+}
+var better = new WithDefaultParams();
+better.calculate(); // Result: 6
